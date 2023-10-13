8b830973ca730d2f76346f2f6637a8e5d9a5fcce
tinotendnyashanu <tinotendanyash@gmail.com>
2023-10-13 03:04:43 +0200

    responsive

--- components/joinus.jsx
+++ components/joinus.jsx
@@ -16,14 +16,14 @@ const EmailForm = ({ onFormSubmit }) => {
   return (
     <motion.form
       onSubmit={handleSubmit}
-      className="flex flex-col sm:flex-row items-center justify-center mb-6"
+      className="flex flex-col sm:flex-row items-center justify-center mb-6 mx-4 sm:mx-0"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
     >
       <input
         type="email"
-        className="py-4 px-14 border rounded-lg mb-2 sm:mb-0 sm:mr-2 w-full sm:w-auto focus:outline-none"
+        className="py-4 px-6 sm:px-8 border rounded-lg mb-2 sm:mb-0 w-full sm:w-auto focus:outline-none"
         placeholder="Enter your email"
         value={email}
         onChange={handleEmailChange}
@@ -31,7 +31,7 @@ const EmailForm = ({ onFormSubmit }) => {
 
       <motion.button
         type="submit"
-        className="bg-red-500 hover:bg-red-700 text-white py-2 px-6 rounded focus:outline-none transition duration-300"
+        className="bg-red-500 hover:bg-red-700 text-white py-2 px-6 sm:px-8 rounded focus:outline-none transition duration-300"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
       >
@@ -47,10 +47,10 @@ const JoinUs = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-primary-black overflow-hidden z-10 ">
-      <div className="p-8 rounded-lg shadow-lg text-center">
+    <div className="min-h-screen flex items-center justify-center bg-primary-black overflow-hidden z-10">
+      <div className="p-8 rounded-lg shadow-lg text-center max-w-lg w-full">
         <motion.h1
-          className="text-8xl font-bold text-white mb-6"
+          className="text-6xl lg:text-8xl font-bold text-white mb-6"
           initial={{ y: -250 }}
           animate={{ y: -10 }}
           transition={{ delay: 0.2 }}
@@ -58,7 +58,7 @@ const JoinUs = () => {
           Join Our Newsletter
         </motion.h1>
         <motion.p
-          className="text-2xl text-gray-400 mb-8"
+          className="text-xl lg:text-2xl text-gray-400 mb-8"
           initial={{ y: -250 }}
           animate={{ y: -10 }}
           transition={{ delay: 0.4 }}
@@ -68,7 +68,7 @@ const JoinUs = () => {
 
         <EmailForm onFormSubmit={handleNotifyClick} />
         <motion.p
-          className="text-xl text-gray-500 text-sm"
+          className="text-lg lg:text-xl text-gray-500 text-sm"
           initial={{ y: -250 }}
           animate={{ y: -10 }}
           transition={{ delay: 0.6 }}
