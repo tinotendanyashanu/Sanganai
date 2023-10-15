74400cec2c24e1ce38369ab92921d829f4ea9473
tinotendnyashanu <tinotendanyash@gmail.com>
2023-10-15 05:37:03 +0200

    SEO

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/Navbar";
+
 import Footer from "../components/Footer";
 
 const Layout = ({ children }) => {
--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,23 @@ import "../styles/globals.css";
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>Sanganai</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Sanganai </title>
+      <meta
+        name="description"
+        content="Sanganai Management Suite: Streamline your event ticketing process, enhance customer experience, and gain valuable insights for event organizers. Buy and sell event tickets online with ease."
+      />
+      <meta
+        name="keywords"
+        content="Sanganai, event tickets, ticketing system, buy tickets, sell tickets, event management"
+      />
+      <meta name="author" content="DigitalGeeks" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link
         rel="stylesheet"
         href="https://stijndv.com/fonts/Eudoxus-Sans.css"
       />
+      {/* Additional CSS or JavaScript files can be added here */}
     </Head>
     <Layout>
       <Component {...pageProps} />
