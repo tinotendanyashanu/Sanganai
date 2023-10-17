9a9627e7bae62273b7666ffad1fc97163d3815a7
tinotendnyashanu <tinotendanyash@gmail.com>
2023-10-17 02:18:30 +0200

    bug/ solial media linkes fixed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -43,17 +43,23 @@ const Footer = () => {
           <div className="flex items-center justify-between flex-wrap gap-4">
             <h4 className="font-extrabold text-[24px] text-white">Sanganai</h4>
             <p className="font-normal text-[14px] text-white opacity-50">
-              Copyright © 2023 Sanganai. All rights reserved.
+              Copyright © 2023 Sanganai & DigitalGeeks. All rights reserved.
             </p>
 
             <div className="flex gap-4">
               {socials.map((social) => (
-                <img
+                <a
                   key={social.name}
-                  src={social.url}
-                  alt={social.name}
-                  className="w-[24px] h-[24px] object-contain cursor-pointer"
-                />
+                  href={social.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={social.icon}
+                    alt={social.name}
+                    className="w-[24px] h-[24px] object-contain cursor-pointer"
+                  />
+                </a>
               ))}
             </div>
           </div>
